b89499cf355934d65fed184981b103a5321a7ea9
Mehmet <haciarifyildiz@gmail.com>
2021-04-10 18:22:46 +0200

    Ucuncu Gonderim

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,6 +5,7 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Versiyon 1");
 		System.out.println("2. degisikligim");
+		System.out.println("3. degisiklik");
 		
 	}
 }

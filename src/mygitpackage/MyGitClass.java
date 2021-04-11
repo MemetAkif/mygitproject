98fa63a62c14030e3e4f3a3e1188799586ec8ac7
Mehmet Akif Yuksel <67182475+MemetAkif@users.noreply.github.com>
2021-04-11 16:27:49 +0200

    Update MyGitClass.java
    Githupta yeni versiyon ekledim

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -6,6 +6,7 @@ public class MyGitClass {
 		System.out.println("Versiyon 1");
 		System.out.println("2. degisikligim");
 		System.out.println("3. degisiklik");
+		System.out.println("GitHuptan 1. degisiklik");
 		
 	}
 }
